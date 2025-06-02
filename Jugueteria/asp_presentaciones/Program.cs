fbb822b292110e104c16b97280da74980145b639
AndresHolguin <ho9arang@gmail.com>
2025-06-02 04:10:00 -0500

    program y startup

---
+++ Jugueteria/asp_presentaciones/Program.cs
@@ -0,0 +1,59 @@
+using lib_presentaciones.Interfaces;
+using lib_presentaciones.Implementaciones;
+using asp_presentaciones;
+using Microsoft.EntityFrameworkCore;
+using lib_repositorios.Implementaciones;
+using asp_servicios;
+var builder = WebApplication.CreateBuilder(args);
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder, builder.Services);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+});
+
+// Registro de tus servicios de presentación
+builder.Services.AddScoped<IVentasPresentacion, VentasPresentacion>();
+builder.Services.AddScoped<IEmpleadosPresentacion, EmpleadosPresentacion>();
+builder.Services.AddScoped<IPermisosPresentacion, PermisosPresentacion>();
+builder.Services.AddScoped<IRolesPresentacion, RolesPresentacion>();
+builder.Services.AddScoped<IUsuariosPresentacion, UsuariosPresentacion>();
+builder.Services.AddScoped<IProoveedoresPresentacion, ProveedoresPresentacion>();
+builder.Services.AddScoped<IJuguetesPresentacion, JuguetesPresentacion>();
+builder.Services.AddScoped<IDetallesVentaPresentacion, DetallesVentaPresentacion>();
+builder.Services.AddScoped<IEstantesPresentacion, EstantePresentacion>();
+builder.Services.AddScoped<IPedidosPresentacion, PedidosPresentacion>();
+builder.Services.AddDbContext<DbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+var app = builder.Build();
+startup.Configure(app, app.Environment);
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+app.UseSession();
+
+app.MapRazorPages();
+app.MapControllers(); // Si estás usando controladores
+
+app.Run();
---
+++ Jugueteria/asp_presentaciones/Startup.cs
@@ -0,0 +1,55 @@
+﻿using lib_presentaciones.Implementaciones;
+using lib_presentaciones.Interfaces;
+
+namespace asp_presentaciones
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public static IConfiguration? Configuration { set; get; }
+
+        public void ConfigureServices(WebApplicationBuilder builder, IServiceCollection services)
+        {
+            // Presentaciones
+            builder.Services.AddScoped<IVentasPresentacion, VentasPresentacion>();
+            services.AddScoped<IEmpleadosPresentacion, EmpleadosPresentacion>();
+            builder.Services.AddScoped<IPermisosPresentacion, PermisosPresentacion>();
+            builder.Services.AddScoped<IRolesPresentacion, RolesPresentacion>();
+            builder.Services.AddScoped<IUsuariosPresentacion, UsuariosPresentacion>();
+            builder.Services.AddScoped<IProoveedoresPresentacion, ProveedoresPresentacion>();
+            builder.Services.AddScoped<IJuguetesPresentacion, JuguetesPresentacion>();
+            builder.Services.AddScoped<IDetallesVentaPresentacion, DetallesVentaPresentacion>();
+            builder.Services.AddScoped<IEstantesPresentacion, EstantePresentacion>();
+            builder.Services.AddScoped<IRolesPresentacion, RolesPresentacion>();
+            builder.Services.AddScoped<IPedidosPresentacion, PedidosPresentacion>(); // Asegúrate de que "PedidosPresentacion" sea el nombre de tu clase de implementación
+
+
+
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddRazorPages();
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+            });
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.MapRazorPages();
+            app.UseSession();
+            app.Run();
+        }
+    }
+}
